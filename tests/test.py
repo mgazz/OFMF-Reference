d1a04f3e8c7246c784ee4dc0b9b0f848dfa1b839
MICHELE GAZZETTI <Michele.Gazzetti1@ibm.com>
2023-10-16 12:54:23 +0100

    update tests
    Signed-off-by: MICHELE GAZZETTI <Michele.Gazzetti1@ibm.com>

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,12 @@
 import test_templates
 import unittest
+import json
 from api_emulator.resource_manager import ResourceManager
+from api_emulator.redfish.constants import PATHS
+
 import g
 
-REST_BASE = '/redfish/v1'
+REST_BASE = '/redfish/v1/'
 g.rest_base = REST_BASE
 
 class TestOFMF(unittest.TestCase):
@@ -13,40 +16,45 @@ class TestOFMF(unittest.TestCase):
         global REST_BASE
         global TRAYS
         global SPEC
+
+        PATHS['Root']= 'Resources/Sunfish'
         resource_manager = ResourceManager(None, None, None, "Disable", None)
         g.app.testing = True
         cls.client = g.app.test_client()
 
     def test_create_computer_system(self):
-        system_url = f"{REST_BASE}/Systems"
+        system_url = f"{REST_BASE}Systems"
         response = self.client.post(system_url, json=test_templates.test_system)
         status_code = response.status_code
         self.assertEqual(status_code, 200)
 
     def test_create_chassis(self):
-        chassis_url = f"{REST_BASE}/Chassis"
+        chassis_url = f"{REST_BASE}Chassis"
         response = self.client.post(chassis_url, json=test_templates.test_chassis)
         status_code = response.status_code
         self.assertEqual(status_code, 200)
     
-    def test_aggregation_source_creation (self):
+    def test_agent_registration(self):
         events_url = f"/EventListener"
         response = self.client.post(events_url, json=test_templates.test_aggregation_source_event)
         status_code = response.status_code
         self.assertEqual(status_code, 200)
         
         manager_name = test_templates.test_aggregation_source_event["Events"][0]["OriginOfCondition"]["@odata.id"].split('/')[-1]
-        aggr_source_url = f"{REST_BASE}/AggregationService/AggregationSources/{manager_name}"
+        conn_method = test_templates.test_aggregation_source_event["Events"][0]["OriginOfCondition"]
+        aggr_source_url = f"{REST_BASE}AggregationService/AggregationSources"
         response = self.client.get(aggr_source_url)
-        status_code = response.status_code
-        self.assertEqual(status_code, 200)
-        response_payload = response.get_json()
 
-        #Check Aggregtion SOurce got created with proper Id
-        self.assertEqual(response_payload["Id"], manager_name)
-        #Check the Hostoname:port was properly parsed
-        hostname = test_templates.test_aggregation_source_event['Events'][0]['MessageArgs'][1]
-        self.assertEqual(response_payload["HostName"], hostname)
+        # validate the generation of a new AggregationSource related to the new Agent 
+        aggr_source_collection = json.loads(response.data)
+        aggr_source_found = False
+        for aggr_source in aggr_source_collection['Members']:
+            aggr_source_endpoint=self.client.get(aggr_source['@odata.id'])
+            aggr_source_data = json.loads(aggr_source_endpoint.data)
+            if conn_method == aggr_source_data['Links']['ConnectionMethod']:
+                aggr_source_found=True
+
+        self.assertTrue(aggr_source_found)
 
 if __name__ == '__main__':
     unittest.main()
